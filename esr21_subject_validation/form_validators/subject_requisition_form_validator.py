b2b3c22e1b75a1229be8f840e9538dca7655cfab
Moses Chawawa <moseschawawa@gmail.com>
2021-09-10 16:20:00 +0200

    modified validation checks--amend

--- esr21_subject_validation/form_validators/subject_requisition_form_validator.py
+++ esr21_subject_validation/form_validators/subject_requisition_form_validator.py
@@ -1,4 +1,4 @@
-from edc_constants.constants import OTHER
+from edc_constants.constants import NOT_APPLICABLE
 from edc_form_validators import FormValidator
 from .crf_form_validator import CRFFormValidator
 
@@ -10,5 +10,5 @@ class SubjectRequisitionFormValidator(CRFFormValidator, FormValidator):
 
         self.validate_other_specify(field='reason_not_drawn')
         self.validate_other_specify(field='item_type')
-        self.required_if(OTHER, field='item_type', field_required='item_count')
+        self.not_required_if(NOT_APPLICABLE, field='item_type', field_required='item_count')
         self.required_if('urgent', field='priority', field_required='urgent_specify')
