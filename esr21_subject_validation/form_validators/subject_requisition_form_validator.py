577826d04a804a83a256aedbbe612ea3a4b287ca
Moses Chawawa <moseschawawa@gmail.com>
2021-09-13 12:15:48 +0200

    Update subject_requisition_form_validator.py

--- esr21_subject_validation/form_validators/subject_requisition_form_validator.py
+++ esr21_subject_validation/form_validators/subject_requisition_form_validator.py
@@ -1,3 +1,4 @@
+from edc_constants.constants import NOT_APPLICABLE
 from edc_form_validators import FormValidator
 from .crf_form_validator import CRFFormValidator
 
@@ -8,7 +9,6 @@ class SubjectRequisitionFormValidator(CRFFormValidator, FormValidator):
         super().clean()
 
         self.validate_other_specify(field='reason_not_drawn')
-
         self.validate_other_specify(field='item_type')
-
+        self.not_required_if(NOT_APPLICABLE, field='item_type', field_required='item_count')
         self.required_if('urgent', field='priority', field_required='urgent_specify')
