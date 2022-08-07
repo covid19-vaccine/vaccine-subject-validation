bbe0df113b62ecd219e149627badf39fda8b5bdb
samKenpachi011 <samuelkabelo1@gmail.com>
2022-08-07 08:41:02 +0200

    updated form validations and tests

--- esr21_subject_validation/form_validators/protocol_deviations_form_validator.py
+++ esr21_subject_validation/form_validators/protocol_deviations_form_validator.py
@@ -1,14 +1,24 @@
 from edc_form_validators import FormValidator
-
+from django.core.exceptions import ValidationError
 
 class ProtocolDeviationFormValidator(FormValidator):
     
-    
     def clean(self):
         super().clean()
-
-    # subject identifier required when deviation name is given
-        self.required_if_not_none(
-            field='deviation_name',
-            field_required='subject_identifiers',
-            required_msg='Please select an identifier')
+    
+        required_fields = ['deviation_form_name','deviation_name',
+                           'subject_identifiers','deviation_description']
+        
+        for r_field in required_fields:
+            self.required_if_not_none(
+                field='deviation_name',
+                field_required=r_field)
+            
+        deviation_name = self.cleaned_data.get('deviation_name')
+        if deviation_name is None:
+            message = {'deviation_name':
+                           ('A protocol deviation name has to be provided')}
+            raise ValidationError(message)
+            
+                 
+            
--- esr21_subject_validation/tests/test_protocol_deviations_form.py
+++ esr21_subject_validation/tests/test_protocol_deviations_form.py
@@ -1,25 +1,67 @@
 from django.test import TestCase, tag
+from edc_base.utils import get_utcnow
 from django.core.exceptions import ValidationError
 from ..form_validators import ProtocolDeviationFormValidator
 
 @tag('pdev')
 class TestProtocolDeviationsForm(TestCase):
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(ProtocolDeviationFormValidator, *args, **kwargs)
-
     def setUp(self):
         self.options = {
-            'deviation_name': None,
+            'report_datetime':  get_utcnow().date(),
+            'deviation_name': 'test',
+            'subject_identifiers': 'test',
+            'deviation_description':'test',
+            'deviation_form_name': 'test',
+            'comment':'test'
         }
-    
-    def test_covid_symptoms_invalid(self):
-        """ Assert that if the participant has had prior covid infection then symptoms
-         are required.
+        
+        
+    def test_deviations_valid(self):
+        """ Test to check if the form is valid .
         """
-        self.options['deviation_name'] = 'test'
-
         form_validator = ProtocolDeviationFormValidator(
-            cleaned_data=self.medical_history_options)
+            cleaned_data=self.options)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError unexpectedly raised. Got{e}')   
+            
+            
+    def test_deviation_name_invalid(self):
+        
+        cleaned_data = {
+            'report_datetime':  get_utcnow().date(),
+            'deviation_name': None,
+            'subject_identifiers': None,
+            'deviation_description': None,
+            'deviation_form_name': None,
+            'comment':'test'
+        }
+        form_validator = ProtocolDeviationFormValidator(cleaned_data=cleaned_data)
+
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('deviation_name', form_validator._errors)
+        
+    def test_subject_idxs_required(self):
+        self.options.update({'subject_identifiers': None})
+        form_validator = ProtocolDeviationFormValidator(cleaned_data=self.options)
+
         self.assertRaises(ValidationError, form_validator.validate)
         self.assertIn('subject_identifiers', form_validator._errors)
+        
+    def test_deviation_form_name_required(self):
+        self.options.update({'deviation_form_name': None})
+        form_validator = ProtocolDeviationFormValidator(cleaned_data=self.options)
+
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('deviation_form_name', form_validator._errors)
+        
+    def test_deviation_desc_required(self):
+        self.options.update({'deviation_description': None})
+        form_validator = ProtocolDeviationFormValidator(cleaned_data=self.options)
+
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('deviation_description', form_validator._errors)
+            
+                 
